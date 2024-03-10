9b311a989bd6017a935792c9a85d48b80738f41e
AlGuron <blizgur8@gmail.com>
2024-03-10 12:57:56 +0200

    new exc

---
+++ src/main/java/telran/probes/exceptions/SensorIllegalStateException.java
@@ -0,0 +1,10 @@
+package telran.probes.exceptions;
+
+@SuppressWarnings("serial")
+public class SensorIllegalStateException extends IllegalStateException {
+
+	public SensorIllegalStateException(String message) {
+		super(message);
+
+	}
+}
